e6406672bbf5f6ec2b57ee22419db21b751830f5
Ahmad Wilson <zer0ne.io.x@gmail.com>
2022-03-17 19:18:23 -0400

    add api request helper functionality to lodestar-helper

--- scripts/lodestar-helper.py
+++ scripts/lodestar-helper.py
@@ -20,11 +20,19 @@ def cli(debug):
 def config():
     pass
 
+@cli.group()
+def status():
+    pass
+
 ###
 # Commands for application configuration customization and inspection
 ###
 
 DEFAULT_LODESTAR_CONFIG_PATH = "{dir}/config.yml".format(dir=os.environ.get("CONF_DIR", "/etc/lodestar"))
+DEFAULT_API_HOST_ADDR = 'http://localhost:9596'
+DEFAULT_API_METHOD = 'GET'
+DEFAULT_API_PATH = 'eth/v1/node/identity'
+DEFAULT_API_DATA = '{}'
 
 
 def print_json(json_blob):
@@ -78,6 +86,51 @@ def customize(config_path):
     # remove surrounding quotes from ALL list setting values if necessary
     subprocess.call(['sed -i "s/\'\[/\[/g" {path}'.format(path=config_path)], shell=True)
     subprocess.call(['sed -i "s/\]\'/\]/g" {path}'.format(path=config_path)], shell=True)
+
+@status.command()
+@click.option('--host-addr',
+              default=lambda: os.environ.get("API_HOST_ADDR", DEFAULT_API_HOST_ADDR),
+              show_default=DEFAULT_API_HOST_ADDR,
+              help='Lodestar beacon client Eth2 API host address in format <protocol(http/https)>://<IP>:<port>')
+@click.option('--api-method',
+              default=lambda: os.environ.get("API_METHOD", DEFAULT_API_METHOD),
+              show_default=DEFAULT_API_METHOD,
+              help='HTTP method to execute a part of request')
+@click.option('--api-path',
+              default=lambda: os.environ.get("API_PATH", DEFAULT_API_PATH),
+              show_default=DEFAULT_API_PATH,
+              help='Restful API path to target resource')
+@click.option('--api-data',
+              default=lambda: os.environ.get("API_DATA", DEFAULT_API_DATA),
+              show_default=DEFAULT_API_DATA,
+              help='Restful API request body data included within POST requests')
+def api_request(host_addr, api_method, api_path, api_data):
+    """
+    Execute RESTful API HTTP request
+    """
+
+    try:
+        if api_method.upper() == "POST":
+            resp = requests.post(
+                "{host}/{path}".format(host=host_addr, path=api_path),
+                json=json.loads(api_data),
+                headers={'Content-Type': 'application/json'})
+        else:
+            resp = requests.get("{host}/{path}".format(host=host_addr, path=api_path))
+
+        # signal error if non-OK response status
+        resp.raise_for_status()
+
+        print_json(resp.json())
+    except requests.exceptions.RequestException as err:
+        sys.exit(print_json({
+            "error": "API request to {host} failed with: {error}".format(
+                host=host_addr,
+                error=err
+            )
+        }))
+    except json.decoder.JSONDecodeError:
+        print(resp.text)
 
 
 if __name__ == "__main__":
